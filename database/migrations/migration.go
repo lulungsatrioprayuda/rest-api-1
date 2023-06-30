8ad56b456a2248a7590f55d958b9eb0a057434f2
lulung <alungsatrio12@gmail.com>
2023-06-30 18:44:17 +0700

    create migration and user model for generate table in database

--- database/migrations/migration.go
+++ database/migrations/migration.go
@@ -1,1 +1,16 @@
 package migrations
+
+import (
+	"fmt"
+
+	"github.com/lulungsatrioprayuda/rest-api-1/database/connection"
+	"github.com/lulungsatrioprayuda/rest-api-1/models"
+)
+
+func Migrate() {
+	err := connection.DB.AutoMigrate(&models.User{})
+	if err != nil {
+		fmt.Println("Error Migrate")
+	}
+	fmt.Println("Success Migrate")
+}
--- models/user_model.go
+++ models/user_model.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type User struct {
+	ID       uint      `gorm:"primary_key" json:"id"`
+	Name     string    `grom:"type:varchar(128);not null" json:"name"`
+	Email    string    `gorm:"type:varchar(128);not null;unique" json:"email"`
+	Password string    `gorm:"type:varchar(128);not null" json:"password"`
+	CreateAt time.Time `gorm:"autoCreateTime" json:"create_at"`
+	UpdateAt time.Time `gorm:"autoUpdateTime" json:"update_at"`
+}
