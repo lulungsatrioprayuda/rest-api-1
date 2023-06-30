4dfce7facde3ba7039a7896d1b363dd3d75aaa02
lulung <alungsatrio12@gmail.com>
2023-06-30 18:37:16 +0700

    update

--- database/connection/database.go
+++ database/connection/database.go
@@ -1,1 +1,21 @@
 package connection
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func DatabseConnection() {
+	var err error
+	dsn := "root:@tcp(127.0.0.1:3306)/fiber_rest_1?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("tidak bisa konek ke database karena" + err.Error())
+	}
+
+	fmt.Println("Berhasil konek ke database")
+}
--- server/main.go
+++ server/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/lulungsatrioprayuda/rest-api-1/database/connection"
 	"github.com/lulungsatrioprayuda/rest-api-1/routes"
 )
 
 func main() {
+
+	connection.DatabseConnection()
 
 	app := fiber.New()
 	routes.SetupRoutes(app)
